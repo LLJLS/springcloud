f6ad9b13f3dc7204416038cffee5b27c54e86430
Kevin <1727101542@qq.com>
2021-03-08 16:59:10 +0800

    canal

---
+++ canal/src/main/java/com/kevin/canal/CanalApp.java
@@ -0,0 +1,12 @@
+package com.kevin.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApp.class,args);
+    }
+}
